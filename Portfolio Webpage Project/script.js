5e6e22e32a3e8052e66a33eca303a208d85b29dc
Saptadip Das <121711803+DragosGitHub@users.noreply.github.com>
2023-09-15 19:42:51 +0530

    Add files via upload

---
+++ Portfolio Webpage Project/script.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  
+  if (menu.classList.contains("open")) {
+    menu.classList.remove("open");
+    icon.classList.remove("open");
+  } else {
+    menu.classList.add("open");
+    icon.classList.add("open");
+  }
+}
